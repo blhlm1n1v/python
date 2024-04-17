ab948fee58b302c76d62d0e3b1c5f2e3085368f0
blhlm1n1v <lalabhmn@gmail.com>
2024-04-17 13:17:50 +0400

    calendar

---
+++ simple_calendar.py
@@ -0,0 +1,7 @@
+import calendar  
+# Enter the month and year  
+yy = int(input("İli daxil edin: "))  
+mm = int(input("Ayı daxil edin: "))  
+  
+# display the calendar  
+print(calendar.month(yy,mm)) 
