fce9d7e68b64b41773f11a778d005719f2d0c273
blhlm1n1v <lalabhmn@gmail.com>
2024-04-17 13:00:08 +0400

    lambda and random simple task

---
+++ lambda.py
@@ -0,0 +1,12 @@
+# #function
+# def multiply(x):
+#     return x*3
+
+# print(multiply(27))
+
+
+
+# declare a lambda function
+multiply = lambda x: x*3
+# call lambda function
+print(multiply(273))
---
+++ randomfromlist.py
@@ -0,0 +1,4 @@
+import random
+
+my_list = [381, 'apple', 965, 'book', 'z', 77]
+print(random.choice(my_list))
